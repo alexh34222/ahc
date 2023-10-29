e2291aac3e7ed26c900682f1284f42e420e74707
alex h <alexander.hallpnw2@icloud.com>
2023-10-28 21:18:23 -0700

    updated projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Fitness from "../assets/images/intuitive.png";
 import "../assets/css/Projects.css";
 
 export default function Projects() {
@@ -8,7 +9,24 @@ export default function Projects() {
         <Header />
       </div>
       <div className="projectsContent">
-        <h1>COMING SOON....</h1>
+        <div className="projectPageTitle">
+          <h1>MY PROJECTS</h1>
+        </div>
+        <div className="projectList">
+          <a href="https://intuitive-fitness.vercel.app/" target="_blank">
+            <div className="intuitiveFitProject projectItem">
+              {" "}
+              <img
+                src={Fitness}
+                alt="screenshot of Intuitive Fitness website"
+              ></img>
+              <div className="projectListText">
+                {" "}
+                <p>Intuitive Fitness</p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
